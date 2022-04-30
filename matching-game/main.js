37a8832a6ec8b317eea7f6421e4c8ba26049fe8e
Felipe <felipe_augusto.bn@hotmail.com>
2022-04-30 10:40:00 -0300

    first commit

---
+++ matching-game/main.js
@@ -0,0 +1,15 @@
+import '../src/styles/generic/reset.css'
+import './style.css'
+import '../src/styles/settings/colors.css'
+import CardGame from '../src/components/CardGame'
+import '../src/styles/elements/base.css'
+
+
+const $root = document.querySelector('#root');
+
+
+const $htmlCardGame = CardGame();
+
+console.log($htmlCardGame);
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,16 @@
+import '../CardGame/style.css';
+
+
+//JSX
+function CardGame(){
+    return `
+        <article class="card-game">        
+        <img src="/images/alura-pixel.png" alt="ee">
+        </article>
+    `;
+
+}
+
+//Serve para exportar a função
+export default CardGame;
+
